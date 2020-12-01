9b11fb940cfa5a6e86ab286307d2b31b5ab47608
Mufaddal Shakir <muffi606@gmail.com>
2020-12-01 17:32:19 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class MyComponent extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			data: null,
+			status: null
+		};
+	}
+
+	componentDidMount(){
+		fetch("https://dog.ceo/api/breeds/image/random")
+		.then(res => res.json())
+		.then(
+			(result) => 
+				this.setState({
+					data: result.message,
+					status: result.status
+				})
+			);
+	}
+
+	render(){
+		const {data,success} = this.state;
+		return(
+			<h1> Guess Which Dog Is This 
+
+
+			<div>
+			 <img src={data} />
+			</div>
+			
+			</h1>
+			)
+	}
+}
+
+
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('root')
+);
