a6871dc351e98a44e4f444ace0e56c21bb38b3ec
Mufaddal Shakir <muffi606@gmail.com>
2020-12-02 15:02:51 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,34 +7,61 @@ class MyComponent extends React.Component {
 		super(props);
 		this.state = {
 			data: null,
-			status: null
+      		status: null,
+          	doggoName: ''
+     
 		};
-	}
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+}
 
 	componentDidMount(){
 		fetch("https://dog.ceo/api/breeds/image/random")
 		.then(res => res.json())
 		.then(
-			(result) => 
+			(result) => {
 				this.setState({
-					data: result.message,
-					status: result.status
-				})
+					data:      result.message,
+          			status:    result.status
+        }) 
+      },
+      (error) => {
+        this.setState({
+          error
+        });
+      }
 			);
-	}
+  }
+
+  handleChange(event){
+    
+  }
+  
+  handleSubmit(event){
+  	console.log(this.state.data);
+    event.preventDefault();
+    this.setState({doggoName: this.state.data});
+  }
 
 	render(){
-		const {data,success} = this.state;
-		return(
-			<h1> For my Baby Saks, with all the love I have, 
-			Guess Which Dog Is This 
+    const {data, doggoName} = this.state;
+    
+    return(
+    
+      <div>
+      <form onSubmit = {this.handleSubmit}>
+        <h1> Baby Saks, Check this: type something and press submit button:
+        Guess Which Dog Is This</h1>
+        <img src={data} />
+        <br></br>
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <input type="submit" value="submit"  />
+      </form>
+          
+         <div> <h1>{this.state.doggoName}</h1> </div>
 
-
-			<div>
-			 <img src={data} />
-			</div>
-			
-			</h1>
+      </div>
 			)
 	}
 }
